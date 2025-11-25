42cd29328b9468d9586bfd2fb73ae4d418635078
Group3-collab <anongroupproject@gmail.com>
2025-11-25 23:37:38 +0600

    Update main.c

--- main.c
+++ main.c
@@ -6,16 +6,46 @@
 
 void main()
 {
-printf("\n");
+char word[20]="PROGRAMMING";
+    char guess[26]="AEI";
+    char show[20];
+    int i,j,lives=4,len;
+    
+    len=strlen(word);
+    
+    for(i=0;i<len;i++){
+        show[i]='_';
+    }
+    show[len]='\0';
+
+    for(i=0;i<len;i++){
+        for(j=0;j<strlen(guess);j++){
+            if(word[i]==guess[j]){
+                show[i]=word[i];
+            }
+        }
+    }
+    
+    printf("\n");
     printf("  +---+\n");
     printf("  |   |\n");
     printf("  O   |\n");
     printf(" /|\\  |\n");
-    printf(" / \\  |\n");
+    if(lives>0)
+        printf(" /     |\n");
+    else
+        printf(" / \\  |\n");
     printf("      |\n");
     printf("========\n");
-    printf("\nHANGMAN GAME\n");
-    printf("Word: _ _ _ _ _\n");
-    printf("Guessed: A, E, I\n");
-    printf("Lives: 2/6\n");
+    
+    printf("\nHANGMAN\n");
+    printf("Word: ");
+    for(i=0;i<len;i++){
+        printf("%c ",show[i]);
+    }
+    printf("\nGuessed: ");
+    for(i=0;i<strlen(guess);i++){
+        printf("%c ",guess[i]);
+    }
+    printf("\nLives: %d\n",lives);
 }
