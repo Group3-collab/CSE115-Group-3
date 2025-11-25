353dd44032bd19e3a495cf9c90750ea1f8a22625
Group3-collab <anongroupproject@gmail.com>
2025-11-25 23:35:51 +0600

    Update main.c

--- main.c
+++ main.c
@@ -6,5 +6,16 @@
 
 void main()
 {
-
+printf("\n");
+    printf("  +---+\n");
+    printf("  |   |\n");
+    printf("  O   |\n");
+    printf(" /|\\  |\n");
+    printf(" / \\  |\n");
+    printf("      |\n");
+    printf("========\n");
+    printf("\nHANGMAN GAME\n");
+    printf("Word: _ _ _ _ _\n");
+    printf("Guessed: A, E, I\n");
+    printf("Lives: 2/6\n");
 }
