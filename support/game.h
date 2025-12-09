8930d7daba6fa7a86c73a7e852534b44b8cd632c
Group3-collab <anongroupproject@gmail.com>
2025-12-09 22:56:38 +0600

    Update game.h

--- support/game.h
+++ support/game.h
@@ -1,3 +1,29 @@
-void loadRandomWord(char secretWord[]);
+#include <stdio.h>
+#include <string.h>
+
+void loadWord(char secretWord[], char guesses[], int guess_count);
 void playHangman(char secretWord[], int *playerScore);
 void showHangman(int triesLeft);
+
+int check(char c, char guesses[], int guess_count)
+{
+  for (int i =0; i<guess_count; i++){
+      if (guesses[i]==c)
+        return 1;
+    }
+  return 0;
+}
+
+void loadWord(char secretWord[], char guesses[], int guess_count)
+{
+  int size = strlen(secretWord);
+
+  for (int i=0; i<size; i++){
+    if (check(secretWord[i],guesses,guess_count)
+      printf("%c ",secretWord[i]);    //shows correctly guessed letters
+    else
+      printf("_ ");                   //blank spaces for unguessed letters
+  }
+  printf("\n");
+}
+
