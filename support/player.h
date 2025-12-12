6810888925a5406c4e44bc246b2c67ea6bd63cf3
Group3-collab <anongroupproject@gmail.com>
2025-12-12 16:57:58 +0600

    Update player.h

--- support/player.h
+++ support/player.h
@@ -1,1 +1,16 @@
+#ifndef PLAYER_H_INCLUDED
+#define PLAYER_H_INCLUDED
 
+
+void getPlayerName(char *name, int maxLength); // To get the player name for our input :D
+                                                    
+void savePlayerScore(const char *playerName, int score);  // To save score to file 
+
+void displayHighScores(); 
+
+int loadWordsFromFile(const char *filename, const char *wordList[], int *wordCount); 
+
+
+void freeWordList(const char *wordList[], int wordCount); // THis frees memory for word list
+
+#endif // PLAYER_H_INCLUDED
